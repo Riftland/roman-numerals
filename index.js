6c8756e7da5904a3ad94bbeaa5d2be67a2603a32
Molpe <fran.mscr@gmail.com>
2019-09-27 17:25:42 +0200

    green test: convert 9 to IX

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const numberToRoman = number => {
     '2': 'II',
     '3': 'III',
     '4': 'IV',
-    '5': 'V'
+    '5': 'V',
+    '9': 'IX'
   }
-  if (number > 5) {
+  if (number > 5 && number < 9) {
     number -= 5
     roman += conversionTable[5]
     numberToRoman(number)
