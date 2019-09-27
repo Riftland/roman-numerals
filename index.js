838ec69f51444c46363373c4ede3e9c5bd1dca62
Molpe <fran.mscr@gmail.com>
2019-09-27 17:12:16 +0200

    green test: refactor

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
 
 const numberToRoman = number => {
-
-  let i = 0;
-  let roman = ''
-
   const conversionTable = {
-    '1': 'I'
+    '1': 'I',
+    '2': 'II',
+    '3': 'III',
+    '4': 'IV',
+    '5': 'V',
+    '6': 'VI'
   }
-
-  for (let i = 0; i < number; i++) {
-    roman += conversionTable[1]
-  }
-
-  return roman
-
-  switch (number) {
-    case 1:
-      return 'I'
-    case 2:
-      return 'II'
-    case 3:
-      return 'III'
-    case 4:
-      return 'IV'
-    case 5:
-      return 'V'
-    case 6:
-      return 'VI'
-  }
+  return conversionTable[number]
 }
 
 module.exports = numberToRoman
